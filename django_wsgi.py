c6a99889b308e708ed4b94769ac65c4181141c91
root <root@liyongqin.com>
2016-05-16 21:55:01 +0800

    PP

--- django_wsgi.py
+++ django_wsgi.py
@@ -9,5 +9,5 @@ from django.core.wsgi import get_wsgi_application
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Shadowsocks_Website.settings")
 application = get_wsgi_application()
