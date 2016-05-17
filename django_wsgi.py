e4667d8560ebd57c774adedc6fa346da7273c73a
root <root@liyongqin.com>
2016-05-17 20:56:58 +0800

    test_1

--- django_wsgi.py
+++ django_wsgi.py
@@ -5,9 +5,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# 将系统的编码设置为UTF8
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Shadowsocks_Website.settings")
 application = get_wsgi_application()
